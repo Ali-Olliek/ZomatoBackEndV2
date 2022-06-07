6cee83a685cb5c4ed5f47ddb438a74db6dc9bcd3
AliOlliek <olliek95@gmail.com>
2022-06-07 19:35:11 +0300

    Added and Tested Sign Up

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,19 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Users extends Controller
-{
-    public function signUp(Request $request, $user_type_id){
-        $user = [];
-        $user["fname"]=$request->fname;
-        $user["lname"]=$request->lname;
-        $user["email"]=$request->email;
-        $user["user_type_id"]=$request->user_type_id;
-        $user["gender"]=$request->gender;
+use App\Models\User;
+
+class UsersController extends Controller {
+
+    public function signUp(Request $request){
+        $user = new User;
+        $user ->name = $request ->name;
+        $user ->email = $request->email;
+        $user ->password = $request ->password;
+        $user ->user_type = $request->user_type_id;
+        $user ->gender=$request->gender;
+        $user -> save();
+        return response()->json([
+            "status"=>"success",
+        ], 200);
+    }
+
+    public function signIn(Request $request){
+        $email = $request->email;
+        $password = $request->password;
+        echo $email, $password;
 
         return response()->json([
             "status"=>"success",
-            "users"=>$user
         ], 200);
     }
 }
