bef41b999fde6832c82ade7e9236d29023f2f338
AliOlliek <olliek95@gmail.com>
2022-06-07 20:14:25 +0300

    Added and Tested Adding Restaurants

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
-class Admins extends Controller
+class AdminsController extends Controller
 {
-    //
+    public function addResto(Request $request){
+        $resto = new Restaurant;
+        $resto -> name = $request -> name;
+        $resto -> cuisine = $request -> cuisine;
+        $resto -> location = $request -> location;
+        $resto->save();
+    }
+
+    public function displayUsers(){
+
+    }
+
+    public function monitorReviews(){
+
+    }
 }
