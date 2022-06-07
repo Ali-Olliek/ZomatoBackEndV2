fcc2f3be88cc6f2a6a566954362848eef7b61bbc
AliOlliek <olliek95@gmail.com>
2022-06-07 19:21:55 +0300

    Adjusted Function Names

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Users Sign Up, Sign In, and Edit Profile //
 
-Route::POST("/SignUp", [UsersController::class, "SignUp"])->name("Sign-up");
-Route::POST("/SignIn", [UsersController::class, "SignIn"])->name("Sign-in");
-Route::POST("/EditProfile", [UsersController::class, "EditProfile"])->name("Edit-prof");
+Route::POST("/SignUp", [UsersController::class, "signUp"])->name("Sign-up");
+Route::POST("/SignIn", [UsersController::class, "signIn"])->name("Sign-in");
+Route::POST("/EditProfile", [UsersController::class, "editProfile"])->name("Edit-prof");
 
 // Admins Add Restaurant, Display Users, Monitor Reviews //
 
-Route::POST("/AddResto", [AdminsController::class, "AddResto"])->name("Add-resto");
-Route::GET("/DisplayUsers", [AdminsController::class, "DisplayUsers"])->name("Display-users");
-Route::GET("/MonitorReviews", [AdminsController::class, "MonitorReviews"])->name("Monitor-reviews");
+Route::POST("/AddResto", [AdminsController::class, "addResto"])->name("Add-resto");
+Route::GET("/DisplayUsers", [AdminsController::class, "displayUsers"])->name("Display-users");
+Route::GET("/MonitorReviews", [AdminsController::class, "monitorReviews"])->name("Monitor-reviews");
 
 // Restaurants Display all, Display Single Restaurant //
 
-Route::GET("/AllRestaurants", [RestaurantsController::class, "DisplayAll"])->name("Display-all");
-Route::GET("/Restaurant", [RestaurantsController::class, "DisplayResto"])->name("Display-resto");
+Route::GET("/AllRestaurants", [RestaurantsController::class, "displayAll"])->name("Display-all");
+Route::GET("/Restaurant", [RestaurantsController::class, "displayResto"])->name("Display-resto");
