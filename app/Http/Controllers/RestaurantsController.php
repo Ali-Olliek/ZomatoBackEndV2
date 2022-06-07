78c4371ce32426ef25cee202c945154b39e88b8f
AliOlliek <olliek95@gmail.com>
2022-06-07 20:07:23 +0300

    Display Restaurants Added

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -4,7 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Restaurants extends Controller
+class RestaurantsController extends Controller
 {
-    //
+    public function displayAll(){
+        $restos = Restaurant::all();
+        return response()->json([
+            "status" => "Success",
+            "restos" => $restos
+        ], 200);
+    }
+
+
+    public function displayResto($name){
+    $resto = Restaurant::where("name", "LIKE", "%$name%")->get();
+    
+    return response()->json([
+        "status" => "Success",
+        "results" => $resto
+    ], 200);
+}
 }
