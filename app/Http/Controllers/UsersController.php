38c6a0052f865da54d7b4f20ebf7b8c457d181bd
AliOlliek <olliek95@gmail.com>
2022-06-07 17:48:46 +0300

    Created SignUp Function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class Users extends Controller
 {
-    //
+    public function signUp(Request $request, $user_type_id){
+        $user = [];
+        $user["fname"]=$request->fname;
+        $user["lname"]=$request->lname;
+        $user["email"]=$request->email;
+        $user["user_type_id"]=$request->user_type_id;
+        $user["gender"]=$request->gender;
+
+        return response()->json([
+            "status"=>"success",
+            "users"=>$user
+        ], 200);
+    }
 }
