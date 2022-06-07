e99b0753a2f7477b417c05370bb07f4a472d34a0
AliOlliek <olliek95@gmail.com>
2022-06-07 20:22:02 +0300

    Added and Tested Users Display

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
+use App\Models\User;
 
-class AdminsController extends Controller
-{
+class AdminsController extends Controller{
+
     public function addResto(Request $request){
         $resto = new Restaurant;
         $resto -> name = $request -> name;
@@ -16,7 +17,11 @@ class AdminsController extends Controller
     }
 
     public function displayUsers(){
-
+        $users = User::all();
+        return response()->json([
+            "status" => "Success",
+            "restos" => $users
+        ], 200);
     }
 
     public function monitorReviews(){
